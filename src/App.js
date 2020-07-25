f56736053a64c421e0480fa866c01b0d6e6f096c
Jean Moraes <moraes.jean91@gmail.com>
2020-07-25 02:21:42 -0300

    enviando os arquivos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [tabela, setTabela] = useState([]);
+  const [input, setInput] = useState([0]);
+  const [erro, setErro] = useState("");
+  
+  let lista = [];
+  let valor = 0;
+
+  function _calcular(){
+
+    if(input <= 0){
+      setErro("Número Inválido!")
+      return;
+    }
+
+    setTabela([]);
+    setErro("");
+    
+    for(let i=1; i<11; i++){
+      valor = input * i;
+      lista.push({fator1: input, fator2: i,  produto: valor});
+    }  
+
+    setTabela(lista);
+    setInput("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="box-input">
+          <h1>CACULADORA DE TABUADAS</h1>
+          <input
+            type="number"
+            value={input}
+            onChange={ e => setInput(e.target.value) }
+          />
+          <span className="erro">{ erro }</span>
+          <button onClick={ () => _calcular() }> CALCULAR </button>
+        </div>
+        
+        <div className="resultado">
+          {
+            tabela.length === 0 ? <h4>Digite um número no campo acima para obter sua tabuada!</h4> : tabela.map((numero, index) => (
+              <p key={index}>
+                {numero.fator1} x {numero.fator2} = {numero.produto}
+              </p>
+            ))
+          }
+        </div>
+      </div>
     </div>
   );
 }
